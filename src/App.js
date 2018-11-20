d30e9a0dc46d1fb8035b66205be2907ded9feeb2
Aleksandr325 <43573566+Aleksandr325@users.noreply.github.com>
2018-11-20 19:22:26 +0200

    nd3| forma

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                nijnjinjinu
+                {getTestArray(5).map(element=>{
+                    return (<div>{element}</div>)
+                })}
+            </div>
+        );
+    }
+}
+
+function getTestArray (count) {
+    let rows = [];
+    for (let i = 0; i < count; i++) {
+        rows.push('Labas' + (i+1))
+    }
+    return rows;
 }
 
 export default App;
+
+
+
+
+
+
+
